a0b35cac076433da2c17b6fa2c2d10d479eb4c75
Kirill Miller <belogorski2000@gmail.com>
2022-08-27 12:06:29 +0300

    Добавьте файлы проекта.

---
+++ _10/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Clock
+    {
+        int hour;
+        int minute;
+        int second;
+
+        pablic Clock(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        public int ToSecond()
+        {
+            return second + minute * 60 + hour * 60 * 60;
+        }
+    }
+}
---
+++ _10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные (часы, минуты, секунды)");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            int minute = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Clock clock = new Clock(hour, minute, second);
+
+            int totalSecond = clock.ToSecond();
+            Console.WriteLine(totalSecond);
+            Console.ReadKey();
+        }
+    }
+}
+
